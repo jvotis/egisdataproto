b531ca3631711826b0dc10081733fb7ea5022a0a
Jim Otis <jvotis2021@gmail.com>
2022-06-05 12:50:23 -0400

    init cdk stack for SQS queue

---
+++ cdk-stacks/molly-cdk-sam/src/main/java/com/myorg/MollyCdkSamStack.java
@@ -0,0 +1,26 @@
+package com.myorg;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.sqs.Queue;
+
+public class MollyCdkSamStack extends Stack {
+    public MollyCdkSamStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public MollyCdkSamStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // The code that defines your stack goes here
+        
+
+
+        // example resource
+        final Queue queue = Queue.Builder.create(this, "EgisDataQueue")
+                 .visibilityTimeout(Duration.seconds(300))
+                 .build();
+    }
+}
